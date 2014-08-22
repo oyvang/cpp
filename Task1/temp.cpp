7ca437252036e706451721f527082a1c87e420be
oyvang <gm.larsen@oyvang.org>
2014-08-23 00:47:40 +0200

    Complite 23-08-14

--- Task1/temp.cpp
+++ Task1/temp.cpp
@@ -1,5 +1,5 @@
 /* 
- * File:   main.cpp
+ * File:   temp.cpp
  * Author: gm
  *
  * Created on August 22, 2014, 2:19 PM
@@ -15,29 +15,29 @@ using namespace std;
  */
 int main(){
     const int antall = 5;
-    int tempUnder =0;
-    int tempBetween=0;
-    int tempOver=0;
+    int tempUnder= 0;
+    int tempBetween = 0;
+    int tempOver = 0;
     double temp = 0;
-    cout << "\nSkriv inn 5 temperaturer\n";
-    for (int i = 0; i < antall; i++){ 
-        cout <<"Temp nr "<< i << ": ";
+    cout << "Skriv inn 5 temperaturer" << endl;
+    for (int i = 0; i < antall; i++){  
+        cout << "Temp nr" << i+1 << ": ";
         cin>>temp;
         if(temp<10){
             tempUnder=tempUnder+1;
         }
-        else if(temp>20){
-            tempOver=tempOver+1;
+        else if(temp>9 && temp<21){
+            tempBetween=tempBetween+1;
         }
         else{
-            tempBetween = tempBetween+1;
+            tempOver=tempOver+1;
         }
     }
    
-cout << "\nTemp under 10 : " << tempUnder << endl;
+cout << "Temp under 10 : " << tempUnder << endl;
 cout << "Temp mellom 10-20 : " << tempBetween << endl;
 cout << "Temp over 20 : " << tempOver << endl;
     
     return 0;
-}
+} 
 
---
+++ Task1/tempFromFile.cpp
@@ -0,0 +1,59 @@
+//
+// tallfil.cpp
+//
+// Programmet leser tall fra fil og skriver summen av tallene til skjermen.
+//
+#include <fstream>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+void lesInnTemp(double temp[], int nrOfTemps);
+
+
+int main() {
+
+    int nrOfTemps = 5;
+    double temp[nrOfTemps];
+    lesInnTemp(temp,nrOfTemps);
+
+return 0;
+}
+
+
+void lesInnTemp(double temp[], int nrOfTemps) {
+    const char filename[] = "tempfile.dat";
+    ifstream file;
+
+    file.open(filename); 
+    if (!file) {
+    cout << "Can not find file!" << endl;
+    exit(EXIT_FAILURE); 
+    }
+    
+    for (int i = 0; i < nrOfTemps; i++) {
+        file >> temp[i];
+    }
+    file.close();
+    
+    int tempUnder= 0;
+    int tempBetween = 0;
+    int tempOver = 0;
+
+    for (int i = 0; i < nrOfTemps; i++){       
+        if(temp[i]<10){
+            tempUnder=tempUnder+1;
+        }
+        else if(temp[i]>20){
+            tempOver=tempOver+1;            
+        }
+        else{
+            tempBetween=tempBetween+1;
+        }
+    }
+    
+    cout << "Temp under 10 : " << tempUnder << endl;
+    cout << "Temp mellom 10-20 : " << tempBetween << endl;
+    cout << "Temp over 20 : " << tempOver << endl;
+
+}
